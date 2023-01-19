eb3e78885544e16936d6198332b85f20bc5c036c
Milena Wasilewska <milena.wasilewska1998@gmail.com>
2023-01-19 18:58:14 +0100

    functions improvement

--- js/script.js
+++ js/script.js
@@ -1,63 +1,87 @@
 {
-    welcome = () => {
+    const welcome = () => {
         console.log("Cześć!");
     };
 
-    const exchangeValue = (exchange) => {
-        const exchangeElement = document.querySelector(".js-exchange");
-        exchangeElement.value = exchange;
-    }
+    const getOutputCurrencyCode = () => {
+        const eurCurrencyElement = document.querySelector(".js-eurCurrency");
+        const usdCurrencyElement = document.querySelector(".js-usdCurrency");
 
-    const calculateResult = (amount, exchange) => {
-        return amount / exchange;
-    }
+        switch (true) {
+            case
+                eurCurrencyElement.checked:
+                return "EUR";
+            case
+                usdCurrencyElement.checked:
+                return "USD";
+            default:
+                return "CHF";
+        };
+    };
+
+    const getExchange = () => {
+        const eurCurrencyElement = document.querySelector(".js-eurCurrency");
+        const usdCurrencyElement = document.querySelector(".js-usdCurrency");
+
+        switch (true) {
+            case
+                eurCurrencyElement.checked:
+                return 4.71;
+            case
+                usdCurrencyElement.checked:
+                return 4.55;
+            default:
+                return 4.77;
+        };
+    };
+
+    let selectCurrency = () => {
+        const selectionFormElement = document.querySelector(".js-selectionForm");
+
+        selectionFormElement.addEventListener("input", () => {
+            const exchangeElement = document.querySelector(".js-exchange");
+            exchange = getExchange();
+            currency = getOutputCurrencyCode();
+            exchangeElement.value = exchange;
+        });
+    };
+
+    const getAmount = () => {
+        const amountElement = document.querySelector(".js-amount");
+
+        return +amountElement.value;
+    };
 
     const changeResultText = (result, currency) => {
-
         const resultElement = document.querySelector(".js-result");
         const resultCurrencyElement = document.querySelector(".js-resultCurrency");
+
+        currency = getOutputCurrencyCode();
 
         resultCurrencyElement.innerText = `Kwota ${currency}: `;
         resultElement.innerText = `${result.toFixed(2)}`;
     };
 
-    const init = () => {
-
+    let calculateResult = () => {
         const formElement = document.querySelector(".js-form");
-        const selectionFormElement = document.querySelector(".js-selectionForm");
-
-        selectionFormElement.addEventListener("input", () => {
-
-            const eurCurrencyElement = document.querySelector(".js-eurCurrency");
-            const usdCurrencyElement = document.querySelector(".js-usdCurrency");
-
-            if (eurCurrencyElement.checked == true) {
-                exchange = 4.71;
-                currency = "EUR";
-            }
-            else if (usdCurrencyElement.checked == true) {
-                exchange = 4.55;
-                currency = "USD";
-            } else {
-                exchange = 4.77;
-                currency = "CHF";
-            }
-
-            exchangeValue(exchange);
-        });
 
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
-    
-            const amountElement = document.querySelector(".js-amount");
-            const amount = +amountElement.value;
-    
-            let result = calculateResult(amount, exchange);
-    
-            changeResultText(result, currency);
-    
+
+            amount = getAmount();
+            exchange = getExchange();
+
+            result = amount / exchange;
+
+            changeResultText(result);
         });
-        
+    };
+
+    const init = () => {
+        selectCurrency();
+
+        calculateResult();
+
         welcome();
     };
 
